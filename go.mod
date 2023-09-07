0f0e2dc07ae6c5fa45a10719d14d31bb18730b8c
MrChip53 <chris@simoni.dev>
2023-09-06 19:49:24 -0500

    add epoll

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module Disgord
+
+go 1.21.1
+
+require golang.org/x/sys v0.12.0
--- internal/pkg/epoll/epoll.go
+++ internal/pkg/epoll/epoll.go
@@ -1,1 +1,64 @@
 package epoll
+
+import (
+	"golang.org/x/sys/unix"
+	"net"
+	"sync"
+)
+
+type Epoll struct {
+	fd          int
+	connections map[int]net.Conn
+	lock        *sync.RWMutex
+}
+
+func New() (*Epoll, error) {
+	fd, err := unix.EpollCreate1(0)
+	if err != nil {
+		return nil, err
+	}
+	return &Epoll{
+		fd:          fd,
+		lock:        &sync.RWMutex{},
+		connections: make(map[int]net.Conn),
+	}, nil
+}
+
+func (e *Epoll) AddConn(conn net.Conn) error {
+	fd := 0 // TODO pull from conn
+	err := unix.EpollCtl(e.fd, unix.EPOLL_CTL_ADD, fd, &unix.EpollEvent{Events: unix.POLLIN | unix.POLLHUP, Fd: int32(fd)})
+	if err != nil {
+		return err
+	}
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	e.connections[fd] = conn
+	return nil
+}
+
+func (e *Epoll) RemoveConn(conn net.Conn) error {
+	fd := 0 // TODO pull from conn
+	err := unix.EpollCtl(e.fd, unix.EPOLL_CTL_DEL, fd, nil)
+	if err != nil {
+		return err
+	}
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	delete(e.connections, fd)
+	return nil
+}
+
+func (e *Epoll) Wait() ([]net.Conn, error) {
+	events := make([]unix.EpollEvent, 100)
+	n, err := unix.EpollWait(e.fd, events, -1)
+	if err != nil {
+		return nil, err
+	}
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	connections := make([]net.Conn, 0, n)
+	for i := 0; i < n; i++ {
+		connections = append(connections, e.connections[int(events[i].Fd)])
+	}
+	return connections, nil
+}
