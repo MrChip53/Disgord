573e8623c3cac74191225eb4d35f71bda150b8c2
MrChip53 <chris@simoni.dev>
2023-09-11 17:31:07 -0500

    add auth functions

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Disgord/internal/pkg/auth"
 	"Disgord/internal/pkg/server"
 	"errors"
 	"github.com/valyala/fasthttp"
@@ -19,7 +20,10 @@ func main() {
 		return nil
 	})
 	srv.GET("/", func(template *template.Template, ctx *fasthttp.RequestCtx) error {
-		err := template.ExecuteTemplate(ctx, "indexPage", nil)
+		dataMap := make(map[string]any)
+		token := ctx.UserValue("token")
+		dataMap["authed"] = token != nil
+		err := template.ExecuteTemplate(ctx, "indexPage", dataMap)
 		if err != nil {
 			log.Print(err)
 			return err
@@ -39,6 +43,62 @@ func main() {
 	})
 	srv.AddErrorTemplate(404, "404Page")
 	srv.AddErrorTemplate(500, "500Page")
+	srv.Use(func(template *template.Template, ctx *fasthttp.RequestCtx) (bool, error) {
+		sessionToken := ctx.Request.Header.Cookie("SessionToken")
+		if len(sessionToken) == 0 {
+			refreshToken := ctx.Request.Header.Cookie("RefreshToken")
+			if len(refreshToken) == 0 {
+				return true, nil
+			}
+
+			refreshPayload, err := auth.VerifyRefreshToken(string(refreshToken))
+			if err != nil {
+				return true, err
+			}
+
+			// TODO fetch user info
+
+			jwtPayload := &auth.JwtPayload{
+				Username: "",
+				Admin:    false,
+				UserId:   refreshPayload.UserId,
+			}
+
+			sToken, rToken, err := auth.GenerateTokens(jwtPayload)
+			if err != nil {
+				return true, err
+			}
+
+			sCookie := &fasthttp.Cookie{}
+			sCookie.SetKey("SessionToken")
+			sCookie.SetValue(sToken)
+			sCookie.SetPath("/")
+			sCookie.SetDomain("")
+			sCookie.SetMaxAge(60)
+			sCookie.SetSameSite(fasthttp.CookieSameSiteStrictMode)
+			sCookie.SetSecure(true)
+			ctx.Response.Header.SetCookie(sCookie)
+			rCookie := &fasthttp.Cookie{}
+			rCookie.SetKey("RefreshToken")
+			rCookie.SetValue(rToken)
+			rCookie.SetPath("/")
+			rCookie.SetDomain("")
+			rCookie.SetMaxAge(60)
+			rCookie.SetSameSite(fasthttp.CookieSameSiteStrictMode)
+			rCookie.SetSecure(true)
+			ctx.Response.Header.SetCookie(rCookie)
+			ctx.SetUserValue("token", jwtPayload)
+		}
+
+		jwtToken, err := auth.VerifyJwtToken(string(sessionToken))
+		if err != nil {
+			return true, nil
+		}
+
+		ctx.SetUserValue("token", jwtToken)
+
+		return true, nil
+	})
 	srv.Use(func(template *template.Template, ctx *fasthttp.RequestCtx) (bool, error) {
 		if string(ctx.Path()) == "/stop" {
 			ctx.SetBody([]byte("stopped"))
--- go.mod
+++ go.mod
@@ -2,12 +2,16 @@ module Disgord
 
 go 1.20
 
-require golang.org/x/sys v0.12.0
+require (
+	github.com/fsnotify/fsnotify v1.6.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/valyala/fasthttp v1.49.0
+	golang.org/x/crypto v0.7.0
+	golang.org/x/sys v0.12.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.49.0 // indirect
 )
